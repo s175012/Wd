600bec3c3951e68c8ca997cce740512728e9dee2
s175012 <147266485+s175012@users.noreply.github.com>
2024-03-13 19:51:47 +0100

    Lab3 tasks

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
+tasks from lab3 now online
+
 moved stuff from other 2 repos
-they will be terminated after 10.03.24
 
 homework for 10.03.24 now online
 
--- lab3-tasks.py
+++ lab3-tasks.py
@@ -1,7 +1,7 @@
 from random import *
 from math import *
 
-t_num = 4
+t_num = 3
 
 match t_num:
     case 1:
@@ -21,7 +21,41 @@ match t_num:
     case 3:
         dic1 = {'pasta': 'pcs', 'tomato': 'kgs', 'cheese': 'kgs', 'sauce': 'pcs'}
         print(dic1)
-        # l1 = [x for x in dic1.keys() if dic1.values() == 'pcs']
-        # print(l1)  # how?
-    # case 4:
-        # def is_tr_rect  # to do
+        l1 = list({x: y for x, y in dic1.items() if y == 'pcs'})
+        print(l1)
+    case 4:
+        def is_tr_rect(a, b, c):
+            print("Triangle", a, ',', b, ',', c)
+            a **= 2
+            b **= 2
+            c **= 2
+            if a + b == c or b + c == a or c + a == b:
+                print('has 90 degree angle.')
+                return 1
+            else:
+                print("doesn't have 90 degree angle.")
+        is_tr_rect(1.5, 2, 2.5)
+    case 5:
+        def tr_ar(a=1.0, b=2.0, h=3.0):
+            print('The area of the trapezium', a, ',', b, ',', h)
+            h *= (a+b)/2
+            print('equals', h)
+        tr_ar()
+        tr_ar(2.5, 6.4, 1.4)
+    case 6:
+        def mul_geo_se(a1=1.0, b=4.0, ile=10):
+            print('Multiplication of geometric sequence', a1, ',', b, ',', ile)
+            for i in range(1, ile):
+                a1 *= b**i
+            print('equals', a1)
+        mul_geo_se()
+        mul_geo_se(1, 2, 1)
+        mul_geo_se(1, 2, 3)
+    case 7:
+        x = float(input('Type positive number: '))
+        print('Real square root from', x)
+        try:
+            x = sqrt(x)
+            print('equals', x)
+        except ValueError:
+            print('does not exist.')
