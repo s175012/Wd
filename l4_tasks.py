91097ca5f54b498c03805beec9776edaf522aa6b
4i02 <106467259+4i02@users.noreply.github.com>
2024-03-28 20:30:41 +0100

    rest of the tasks from numpy1.pdf

--- l4_tasks.py
+++ l4_tasks.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-tn = 3
+tn = 6
 
 match tn:
     case 1:
@@ -16,3 +16,44 @@ match tn:
             m = np.array([range(1, n * n + 1)])
             return m.reshape(n, n)
         print(c_a(6))
+    case 4:
+        def generate_powers(base, degree):
+            return np.logspace(1, degree, num=degree, base=base)
+        print(generate_powers(2, 10))
+    case 5:
+        def generate_diagonal_matrix(row_length):
+            return np.diag(np.linspace(row_length, 1, num=row_length*2))
+        print(generate_diagonal_matrix(3))
+    case 6:
+        word_search = np.matrix([['t', 'a', 'c', 'o'],
+                                 ['a', 's', 'd', 'c'],
+                                 ['o', 'h', 'u', 'o'],
+                                 ['c', 't', 'a', 'n']], dtype=str)
+        print(word_search)
+    case 7:
+        def gen_mul_2_diag_matrix(n):
+            matrix = np.diag(2*np.ones(n))
+            for i in range(1, n):
+                matrix[i:n, 0:n-i] += np.diag((i+1)*2*np.ones(n-i))
+                matrix[0:n-i, i:n] += np.diag((i+1)*2*np.ones(n-i))
+            return matrix
+        print(gen_mul_2_diag_matrix(3))
+    case 8:
+        def divide_array(array, direction):
+            print(type(int(array.shape[0])))
+            if direction == 'v':
+                if array.shape[0] % 2:
+                    return 0
+                return array[0:int(array.shape[0])//2, :], array[int(array.shape[0])//2:, :]
+            if direction == 'h':
+                if array.shape[1] % 2:
+                    return 0
+                return array[:, 0:int(array.shape[0])//2], array[:, int(array.shape[0])//2:]
+        m = np.array((range(2), range(2)), dtype='int8')
+        print(divide_array(m, 'h'))
+    case 9:
+        Fibonacci_matrix = np.array(np.zeros(25), dtype=int)
+        Fibonacci_matrix[1] = 1
+        for i in range(2, 25):
+            Fibonacci_matrix[i] = Fibonacci_matrix[i-1] + Fibonacci_matrix[i-2]
+        print(Fibonacci_matrix.reshape((5, 5)))
